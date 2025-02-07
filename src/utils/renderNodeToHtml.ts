1b596671c05b0a3e6ecb9c4451a180157aacf06d
Gaëtan Denis <gaetandenis@outlook.be>
2025-02-08 00:57:21 +0100

    FEAT🚀: Add HTML rendering for all Markdown heading levels

--- src/utils/renderNodeToHtml.ts
+++ src/utils/renderNodeToHtml.ts
@@ -18,10 +18,26 @@ export function renderNodeToHtml(node: Node) : string {
     if(node.type==="h2"){
         return`<h2>${node.content}</h2>`
     }
-    // Si le type est égal à h1, je retourne l'élément au format html <p>
+    // Si le type est égal à h2, je retourne l'élément au format html <p>
 
     if(node.type==="p"){
         return`<p>${node.content}</p>`
+    }
+
+    if(node.type==="h3"){
+        return`<h3>${node.content}</h3>`
+    }
+
+    if(node.type==="h4"){
+        return`<h4>${node.content}</h4>`
+    }
+
+    if(node.type==="h5"){
+        return`<h5>${node.content}</h5>`
+    }
+
+    if(node.type==="h6"){
+        return`<h6>${node.content}</h6>`
     }
     return ""
 }
