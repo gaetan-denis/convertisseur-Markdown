30c09ba0e26f8d9b06c4ae5599434303d9675106
Gaëtan Denis <gaetandenis@outlook.be>
2025-02-07 18:16:50 +0100

    FEAT🚀 Add Markdown to HTML converter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import {parseMarkdown} from "./utils/MarkdownParser.ts";
+import {renderNodeToHtml} from "./utils/renderNodeToHtml.ts";
 import './App.css'
+import {TextareaHTMLAttributes, useState} from "react";
 
 function App() {
+  const[markdownText, setMarkdownText] = useState<string>("");
 
+  const handleChange = (event: React.ChangeEvent<TextareaHTMLAttributes<string>>) => {
+    setMarkdownText(event.target.value);
+  }
+
+  const generateHtml = () => {
+    const rootNode = parseMarkdown(markdownText);
+    return renderNodeToHtml(rootNode);
+  };
 
   return (
-   <h1>Convertisseur Markdown</h1>
+      <div>
+        <h1>Convertisseur Markdown</h1>
+        <textarea value={markdownText} onChange={handleChange} placeholder="Entrez votre markdown ici" rows={10} cols={50}/>
+        <textarea value={generateHtml()} rows={10} cols={50} readOnly/>
+
+      </div>
   )
 }
 
 export default App
+
+
