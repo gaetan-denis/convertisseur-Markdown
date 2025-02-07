1d247f2676af741181f9b5aedb64cf0a3f8e703c
Gaëtan Denis <gaetandenis@outlook.be>
2025-02-08 00:42:33 +0100

    FIX🔧: Correct heading parsing order in parseMarkdown function

--- src/utils/MarkdownParser.ts
+++ src/utils/MarkdownParser.ts
@@ -10,14 +10,15 @@ export function parseMarkdown(markdown: string): Node {
     let currentParent = root
 
     for(const line of lines){
-        if(line.startsWith("#")){
-            currentParent=new Node("h1", line.trim().slice(2))
+        if(line.startsWith("##")){
+            currentParent=new Node("h2", line.trim().slice(2))
             root.addChild(currentParent);
-        }else if(line.startsWith("##")){
+        }else if(line.startsWith("#")){
 
-            // //TODO : vérifier le souci avec l'annotation ## qui reste en <h1>
+            // TODO : ~~vérifier le souci avec l'annotation ## qui reste en <h1>~~
+            // FIXED : Le souci venait du fait que les titres de niveau 2 entrait aussi dans la condition line.starsWith(#), j'ai donc inversé l'ordre des conditions
 
-            currentParent=new Node("h2", line.trim().slice(3))
+            currentParent=new Node("h1", line.trim().slice(3))
             root.addChild(currentParent);
         }else if(line.trim()){
             currentParent=new Node("p", line.trim())
